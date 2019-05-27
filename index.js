bda49017ab9d9b4e54feb6d1823c594061c1c32f
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-05-26 17:52:48 -0700

    added am/pm functionality

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ function updateTime(){
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
+    let timeOfDay = '';
+
+    if (hours < 12) {
+        timeOfDay = 'AM';
+    } else {
+        timeOfDay = 'PM';
+    }
 
     $('.time-section:first-child').text(hours);
-   
     $('.time-section:nth-child(2)').text(minutes);
-  
-    $('.time-section:last-child').text(seconds);
+    $('.time-section:nth-child(3)').text(seconds);
+    $('#am-pm').text(timeOfDay);
 }
 
 $(document).ready(function() {
     updateTime();
     window.setInterval(updateTime, 1000);
+
   });
